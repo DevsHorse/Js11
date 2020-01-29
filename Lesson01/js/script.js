282558cb69df573a0b69bdc98745150fa159f3eb
Vladislav Holovko <lobsterponi@gmail.com>
2020-01-29 07:10:39 +0300

    Добавил урок 4

--- Lesson01/js/script.js
+++ Lesson01/js/script.js
@@ -1,38 +1,52 @@
 'use strict';
 
-let money = +prompt('Ваш месячный доход?'),
+let money = +prompt('Ваш месячный доход?', 40000),
   income  = 'Фриланс',
-  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'), // доп. расходы
+  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Кафе, такси'), // доп. расходы
   deposit = confirm('Есть ли у вас депозит в банке?'), 
-  mission = 100000,
+  mission = 100000, // Цель 
   period  = 12;  // месяцев
 
-let expenses1 = prompt('Введите обязательную статью расходов?'),
- amount1 = prompt('Во сколько это обойдется?'),
- expenses2 = prompt('Введите обязательную статью расходов?'),
- amount2 = prompt('Во сколько это обойдется?'),
- budgetMonth = money - amount1 - amount2, // Месячный бюджет
- budgetDay = budgetMonth / 30, // Дневной бюджет
- missionSuccess = mission / budgetMonth; // За сколько месяцев соберется mission
+let expenses1 = prompt('Введите обязательную статью расходов?', 'Машина'),
+ amount1 = +prompt('Во сколько это обойдется?', 3000),
+ expenses2 = prompt('Введите обязательную статью расходов?', 'Еда'),
+ amount2 = +prompt('Во сколько это обойдется?', 7000);
 
 
-// Вывод типа данных 
-console.log('Тип money:', typeof(money));
-console.log('Тип income:', typeof(income));
-console.log('Тип deposit:', typeof(deposit));
-// Длина строки
-console.log('Длина строки addExpenses: ',addExpenses.length);
-// Вывод текста и переменных (Конкатинация)
-console.log('Период равен ' + period + ' месяцев.');
-console.log('Цель заработать ' + mission + ' рублей.');
-// Приведение строки к нижнему регистру и разбитие на массив 
-console.log('Массив addExpenses: ', addExpenses.toLowerCase().split(', '));
-// Вывод месячного бюджета
-console.log('Бюджет на месяц: ', budgetMonth);
-// Вывод дневного бюджета 
-console.log('Бюджет на день: ', Math.floor(budgetDay));
-// За сколько месяцев будет достигнута цель
-console.log('Месяцев до достижения цели: ', Math.ceil(missionSuccess));
+//Обязательные расходы за месяц
+function getExpensesMonth() {
+  return amount1 + amount2;
+};
+// Бюджет на месяц
+function getAccumulatedMonth() {
+  return money - getExpensesMonth();
+};
+
+let accumulatedMonth = getAccumulatedMonth(), // месячный бюджет 
+  budgetDay = accumulatedMonth / 30; // Дневной бюджет
+  
+
+//За сколько будет достигнута цель
+function getTargetMonth() {
+  return mission / accumulatedMonth;
+};
+// Показать тип переменной 
+function showTypeOf(variable) {
+  return typeof(variable);
+};
+
+function getStatusIncome() {
+  return income;
+};
+
+console.log('Тип money:', showTypeOf(money)); // Вывод типа данных 
+console.log('Тип income:', showTypeOf(income));
+console.log('Тип deposit:', showTypeOf(deposit));
+console.log('getExpensesMonth: ', getExpensesMonth()); // Расходы за месяц
+console.log('Массив addExpenses: ', addExpenses.toLowerCase().split(', ')); // Вывод возможных расходов
+console.log('getTargetMonth: ', Math.floor(getTargetMonth())); //Срок достижения цели
+console.log('Бюджет на день: ', Math.floor(budgetDay)); // Вывод дневного бюджета 
+console.log('getStatusIncome: ', getStatusIncome()); // Статус income, не особо понятно что там должно быть =)
 
 //Расчет уровня дохода
 if (budgetDay >= 1200) {
@@ -46,3 +60,12 @@ if (budgetDay >= 1200) {
 } else {
   console.log('Что то пошло не так');
 }
+
+
+
+
+
+
+
+
+
