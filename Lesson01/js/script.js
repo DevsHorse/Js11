84dc9ec1f4727c3436a13706c812f1e744714478
Vladislav Holovko <lobsterponi@gmail.com>
2020-02-03 16:32:17 +0300

    Добавил урок 8

--- Lesson01/js/script.js
+++ Lesson01/js/script.js
@@ -21,13 +21,32 @@ let appData = {
     expenses: {},
     addExpenses: [],
     deposit: false,
+    percentDeposit: 0,
+    moneyDeposit: 0,
     mission: 50000,
     period: 5,
     asking: function() {   
-      let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Кафе, такси');
+
+      if (confirm('Если ли у Вас дополнительный источник заработка?')) {
+        let itemIncome,
+            cashIncome;
+        do {
+          itemIncome = prompt('Какой у вас дополнительный заработок', 'Фриланс');
+        }
+        while (itemIncome === null || itemIncome.trim() === '' || isNumber(itemIncome));
+        do {
+          cashIncome = prompt('Сколько в месяц Вы на этом зараатываете?', '10000');
+        }
+        while (!isNumber(cashIncome));
+        
+        appData.income[itemIncome] = +cashIncome;
+      }
+
+      let addExpenses = prompt('Перечислите возможные расходы через запятую', 'КаФе, тАкси, cАдИк');
           appData.addExpenses = addExpenses.toLowerCase().split(', ');
           appData.deposit = confirm('Есть ли у вас депозит в банке?');
-    
+          appData.getInfoDeposit();
+
       let expensesKey,
           keyValue;
 
@@ -84,6 +103,21 @@ let appData = {
             messege = 'К сожалению у вас уровень дохода ниже среднего';
           } 
        return messege;
+    },
+    getInfoDeposit: function() {
+      if (appData.deposit) {
+        do {
+          appData.percentDeposit = prompt('Какой годовой процент?', 10);
+        }
+        while (!isNumber(appData.percentDeposit));
+        do {
+          appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+        }
+        while (!isNumber(appData.moneyDeposit));
+      }
+    },
+    calcSavedMoney: function() {
+      return appData.budgetMonth * appData.period;
     }
 };
 
@@ -99,6 +133,15 @@ for (let key in appData) {
   console.log('Наша программа включает в себя данные: \n' + 
   'Свойство: ' + key + '\n\n Значение: ' + appData[key]);
 }
+
+
+(function getStringOfArr(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i][0].toUpperCase() + arr[i].substring(1, arr[i].length));
+  }
+ return console.log(newArr.join(', '));
+})(appData.addExpenses);
 
 
 
