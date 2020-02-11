0aeef25803c3aa26de7c419009821a9df976702e
Vladislav Holovko <lobsterponi@gmail.com>
2020-02-11 21:10:08 +0300

    added lesson14 №3

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 'use strict';
 
-const DomElement = function(sel, h, w, bg, fz, text) {
+const DomElement = function(sel, h, w, bg) {
   this.selector = sel;
   this.height = h;
   this.width = w;
   this.bg = bg;
-  this.fontSize = fz;
-  this.text = text;
 };
 
 const body = document.querySelector('body');
+body.style.position = 'relative';
 
 DomElement.prototype.createElement = function() {
   let element;
@@ -17,23 +16,46 @@ DomElement.prototype.createElement = function() {
   if (this.selector[0] === '.') {
     element =  document.createElement('div');
     element.classList.add(this.selector.substring(1));
-  } else if (this.selector[0] === '#') {
-    element= document.createElement('p');
-    element.id = this.selector.substring(1);
   }
-
-  element.textContent = this.text;
   body.appendChild(element);
-
   element.style.cssText = `
     height: ${this.height}px;
     width: ${this.width}px;
     background: ${this.bg};
-    font-size: ${this.fontSize}px;
-    text-align: center;
+    position: absolute;
+    top: 200px;
+    left: 800px;
   `;
 };
 
-const newObject = new DomElement('.block', 100, 200, '#666', 22, 'ЭТО НОВЫЙ ОБЬЕКТ');
+const newObject = new DomElement('.block', 100, 100, '#000');
+document.addEventListener('DOMContentLoaded', () => newObject.createElement());
 
-newObject.createElement();
+window.addEventListener('keydown', function(event) {
+  let square = document.querySelector('.block');
+
+  if (event.keyCode === 40) {
+
+      let top = parseFloat(square.style.top);
+      square.style.top = `${top + 10}px`;
+
+  } else if (event.keyCode === 38) {
+
+      let top = parseFloat(square.style.top);
+      square.style.top = `${top - 10}px`;
+
+  } else if (event.keyCode === 37) {
+
+      let left = parseFloat(square.style.left);
+      square.style.left = `${left - 10}px`;
+
+  } else if (event.keyCode === 39) {
+
+      let left = parseFloat(square.style.left);
+      square.style.left = `${left + 10}px`;
+
+  } else {
+    return;
+  }
+
+});
